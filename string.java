ca684d640538a70ec80f93ed0bfbd76b9e8bc3f2
AswinSatheesh <aswinsatheesh2001@gmail.com>
2023-07-11 21:30:21 +0530

    updated

--- string.java
+++ string.java
@@ -7,5 +7,19 @@ class string{
    System.out.println(s1.equals(s2));//true  
    System.out.println(s1.equals(s3));//true  
    System.out.println(s1.equals(s4));//false  
+
+
+
+
+    if (((regex.value.length == 1 &&  
+             ".$|()[{^?*+\\".indexOf(ch = regex.charAt(0)) == -1) ||  
+             (regex.length() == 2 &&  
+              regex.charAt(0) == '\\' &&  
+              (((ch = regex.charAt(1))-'0')|('9'-ch)) < 0 &&  
+              ((ch-'a')|('z'-ch)) < 0 &&  
+              ((ch-'A')|('Z'-ch)) < 0)) &&  
+            (ch < Character.MIN_HIGH_SURROGATE ||  
+             ch > Character.MAX_LOW_SURROGATE))  
+        {  
  }  
 }  
